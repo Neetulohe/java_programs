044cd453893f66708e44dd978c48bb113e65a834
a148512 <a148512@MC0XJE2C.ww930.my-it-solutions.net>
2020-10-07 13:13:04 +0530

    2nd cpommit

--- AutoScript/src/qsp/Browser.java
+++ AutoScript/src/qsp/Browser.java
@@ -24,5 +24,6 @@ public class Browser {
 	    js.executeScript("scrollBy("+300+","+300+")");
          String price = driver.findElement(By.xpath("//span[text()='Haier 181 L 2 Star Direct-Cool Single Door Refrigerator (HED-1812BKS-E, Black Brushline)']/ancestor::div[@class='sg-col-4-of-12 sg-col-8-of-16 sg-col-16-of-24 sg-col-12-of-20 sg-col-24-of-32 sg-col sg-col-28-of-36 sg-col-20-of-28']/descendant::span[@class='a-price-whole']")).getText();
 		System.out.println(price);
+		driver.quit();
 		}
 }
